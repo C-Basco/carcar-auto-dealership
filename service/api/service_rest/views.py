83cef0e53c821efcf887692e1b00820b9a561c41
Amanda <asouthern24@gmail.com>
2023-06-06 15:33:55 -0400

    updated views

--- service/api/service_rest/views.py
+++ service/api/service_rest/views.py
@@ -68,8 +68,12 @@ def api_list_technicians(request):
 @require_http_methods(["DELETE"])
 def api_delete_technician(request, id):
     if request.method == "DELETE":
-        count, _ = Technician.objects.filter(id=id).delete()
-        return JsonResponse({"deleted": count > 0})
+        tech = get_object_or_404(Technician, id=id)
+        tech.delete()
+        return JsonResponse(
+                {"message": "Deleted"},
+                status=200,
+            )
     
 
 @require_http_methods(["GET", "POST"])
